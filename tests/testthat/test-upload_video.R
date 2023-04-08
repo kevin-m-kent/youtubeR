100e243098419992c6a0da53a41e4e74c83fd5eb
Kevin Kent <kevinkent@Kevins-Mac-mini.local>
2023-04-08 12:47:58 -0400

    still can't get the mock test working

--- tests/testthat/test-upload_video.R
+++ tests/testthat/test-upload_video.R
@@ -1,20 +1,26 @@
 test_that("upload_video returns expected video ID with a mocked API", {
+
   # Read the mock response
 
-  json_text <- readLines("mock_upload_response.json") 
-  print(json_text)
-  mock_response <- jsonlite::fromJSON(paste(json_text, collapse = ""))
+
   # Create a custom response object using the mock data
-  mock_response_object <- function(req)  {httr2::response(
+  mock_response_object <- function(req)  {
+
+    browser()
+    
+    json_text <- readLines("mock_upload_response.json")
+    mock_response <- jsonlite::fromJSON(paste(json_text, collapse = ""))
+    
+    httr2::response(
     status = 200,
     headers = list("Content-Type" = "application/json"),
-    body = jsonlite::toJSON(mock_response)
+    body = mock_response
   )
   }
 
   client <- list(client_id = "mockClientId", client_secret = "mockClientSecret")
   snippet <- list(title = "Test Video", description = "This is a test video.")
-  video_path <- "video.mp4"
+  video_path <- "test.txt"
   # Use the custom response object to mock the API call
 
   httr2::with_mock(mock_response_object,  upload_video(client, snippet, video_path))
