95b9c9a0fac13f381fc88db646201ae677fb343a
jimrothstein <jimrothstein@gmail.com>
2024-02-05 11:10:22 -0800

    add function youtubeR::get_my_playlists

--- R/get_my_playlists.R
+++ R/get_my_playlists.R
@@ -3,21 +3,17 @@
 #'  Retrieve a list of playlists where mine=TRUE
 #'  (TODO: refactor to accept mine or any valid channelId)
 #'
-#'  @param mine boolean
-#'  @param max_results The maximum number of results to return.
-#'  @inheritParams yt_call_api
+#' @param mine boolean
+#' @param max_results The maximum number of results to return.
+#' @inheritParams yt_call_api
 #'
-#'  @return A list of playlists, each with title and description.
-#'  @export
+#' @return A list of playlists, each with title and description.
+#' @export
 #'
-#' @examples get_my_playlists()
-#' # from Youtube - get all playlists
-# curl \
-#' #  'https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=25&mine=true&fields=items(snippet(title%2C%20description))&key=[YOUR_API_KEY]' \
-#' #  --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-#' #  --header 'Accept: application/json' \
-#' #--compressed
-# (TODO refactor to allow:  channelId =  "UClB5qWyXejlAwwkDAzJis-Q" # my channel  )
+#' @examplesIf yt_has_client_envvars() && interactive()
+#' get_my_playlists()
+#' #  (TODO refactor to allow:  channelId =  "UClB5qWyXejlAwwkDAzJis-Q" # my channel  )
+#' @export
 get_my_playlists <- function(max_results = 100,
                              client = yt_construct_client(),
                              cache_disk = getOption("yt_cache_disk", FALSE),
@@ -37,11 +33,11 @@ get_my_playlists <- function(max_results = 100,
     cache_key = cache_key,
     token = token
   )
-  # browser()
-  # if (length(res$items)) {
-  #    return(purrr::map(res$items, purrr::pluck, "contentDetails"))
-  #    return(purrr::map(res$items, purrr::pluck, "snippet"))
-  #  } else {
-  #    return(NULL) # nocov
-  #  }
+
+  if (length(res$items)) {
+    #   return(purrr::map(res$items, purrr::pluck, "contentDetails"))
+    return(purrr::map(res$items, purrr::pluck, "snippet"))
+  } else {
+    return(NULL) # nocov
+  }
 }
